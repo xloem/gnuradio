9d7c5acc351cfca9cf7c4923be7b7be4c15e9a35
Håkon Vågsether <hauk142@gmail.com>
2021-01-05 15:25:21 +0100

    grcqt: Partially add very crude ports and connections
    Signed-off-by: Håkon Vågsether <hauk142@gmail.com>

--- grc/core/base.py
+++ grc/core/base.py
@@ -25,7 +25,7 @@ class Element(object):
         """
         del self._error_messages[:]
 
-        for child in self.children():
+        for child in self.child_elements():
             child.validate()
 
     def is_valid(self):
@@ -66,7 +66,7 @@ class Element(object):
         """
         for msg in self._error_messages:
             yield self, msg
-        for child in self.children():
+        for child in self.child_elements():
             if not child.enabled or child.get_bypassed():
                 continue
             for element_msg in child.iter_error_messages():
